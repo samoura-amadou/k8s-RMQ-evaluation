feaedc29bcac41702b43c772a8c7332bd82eee8d
arthur <arthur.escriou@gmail.com>
2021-03-22 18:34:04 +0100

    add middleware

--- server/src/auth/index.js
+++ server/src/auth/index.js
@@ -2,8 +2,9 @@ const fetch = require('node-fetch')
 const { get, post, context } = require('@frenchpastries/assemble')
 const { log } = require('../utils/logger')
 const { loginHandler } = require('./handler')
+const { response } = require('@frenchpastries/millefeuille/response')
 
-const routes = context('/auth', [get('/login', loginHandler)])
+const routes = context('/auth', [get('/', () => response('ok'))])
 
 module.exports = {
   routes,
--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,5 +1,28 @@
-const parseAuthorization = handler => async request => {}
+const check = bearer =>
+  new Promise((resolve, reject) => {
+    log('check ', bearer)
+    jwt.verify(
+      bearer,
+      process.env.AUTH0_PUBLIC_KEY,
+      {
+        algorithms: ['RS256'],
+      },
+      (err, decoded) => (err ? reject(err) : resolve(decoded))
+    )
+  })
+
+const guardAuth = handler => async request => {
+  const { headers, services } = request
+  const authorization = headers.authorization || ''
+  if (authorization.startsWith('Bearer')) {
+    return check(request.headers.authorization.replace(/^Bearer\s+/, ''))
+      .then(decoded => handler({ ...request, authorized: true, decoded }))
+      .catch(err => handler({ ...request, authorized: false }))
+  } else {
+    return handler({ ...request, authorized: false })
+  }
+}
 
 module.exports = {
-  parseAuthorization,
+  guardAuth,
 }
